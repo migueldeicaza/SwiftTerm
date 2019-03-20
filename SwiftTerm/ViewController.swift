a15e7c7c9d3de99b8df3d01c8083f509b4afa713
Miguel de Icaza <miguel@gnome.org>
2019-03-19 22:31:39 -0700

    Initial Commit

---
+++ SwiftTerm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTerm
+//
+//  Created by Miguel de Icaza on 3/19/19.
+//  Copyright © 2019 Miguel de Icaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
