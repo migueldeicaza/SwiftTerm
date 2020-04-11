ffe320ecc93e36f8b2c25218cf75165aa88843a1
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-04-11 19:49:53 +0200

    Separate TerminalViewDelegate

---
+++ SwiftTerm/Sources/SwiftTerm/Mac/TerminalViewDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcin Krzyzanowski on 11/04/2020.
+//
+
+public protocol TerminalViewDelegate: class {
+  /**
+   * The client code sending commands to the terminal has requested a new size for the terminal
+   * Applications that support this should call the `TerminalView.getOptimalFrameSize`
+   * to get the ideal frame size.
+   *
+   * This is needed for the rare cases where the remote client request 80 or 132 column displays,
+   * it is a rare feature and you most likely can ignore this request.
+   */
+  func sizeChanged (source: TerminalView, newCols: Int, newRows: Int)
+
+  /**
+   * Request to change the title of the terminal.
+   */
+  func setTerminalTitle(source: TerminalView, title: String)
+
+  /**
+   * The provided `data` needs to be sent to the application running inside the terminal
+   */
+  func send (source: TerminalView, data: ArraySlice<UInt8>)
+
+  /**
+   * Invoked when the terminal has been scrolled and the new position is provided
+   */
+  func scrolled (source: TerminalView, position: Double)
+}
