a1b1570f077e7330fa517a8a7e66c906a614e116
Miguel de Icaza <miguel@gnome.org>
2020-04-17 18:58:25 -0400

    Fix a porting mistake - when I made the method part of Buffer, I dropped every 'buffer.y' and it became 'y', but that broke, because there is a local y, so it should be 'self.y'.   Also, brings a bug fix that Greg just added to XtermSharp that fixes a crash during reflow

---
+++ SwiftTerm/Tests/SwiftTermTests/ReflowTests.swift
@@ -0,0 +1,37 @@
+//
+//  ReflowTests.swift
+//  
+//
+//  Created by Miguel de Icaza on 4/17/20.
+//
+
+import Foundation
+import XCTest
+
+@testable import SwiftTerm
+
+final class ReflowTests: XCTestCase {
+    
+    func testDoesNotCrashWhenReflowingToTinyWidth ()
+    {
+        let options = TerminalOptions(cols: 10, rows: 10, scrollback: 1)
+        let h = HeadlessTerminal (queue: SwiftTermTests.queue, options: options) { exitCode in }
+        
+        let t = h.terminal!
+        
+        t.feed (text: "1234567890\r\n")
+        t.feed (text: "ABCDEFGH\r\n")
+        t.feed (text: "abcdefghijklmnopqrstxxx\r\n")
+        t.feed (text: "\r\n")
+        
+        // if we resize to a small column width, content is pushed back up and out the top
+        // of the buffer. Ensure that this does not crash
+        t.resize(cols: 3, rows: 10)
+        XCTAssert(true)
+    }
+    
+    static var allTests = [
+          ("testDoesNotCrashWhenReflowingToTinyWidth", testDoesNotCrashWhenReflowingToTinyWidth),
+    ]
+}
+
