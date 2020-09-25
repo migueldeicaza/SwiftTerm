migueldeicaza/SwiftTerm

commit 718a144ef30cc69b85df6bdc65e39de353212a41

reset alt buffer cursor when switching to it

Sources/SwiftTerm/BufferSet.swift
@@ -62,8 +62,10 @@ class BufferSet {
             return
         }
         
-        alt.x = normal.x
-        alt.y = normal.y
+        // less switches to alt buffer without moving cursor and assumes
+        // switching to alt buffer resets cursor
+        alt.x = 0 // normal.x
+        alt.y = 0 // normal.y
         // Since the alt buffer is always cleared when the normal buffer is
         // activated, we want to fill it when switching to it.
         

commit cf7c7f20818134e1b200deedac8387ceaa4c8ec5

Fix the two regressions introduced by 718a144ef30cc69b85df6bdc65e39de353212a41 and 5448b319b5c03a1296b79c604e304e3b5dec0b1c, see details in https://github.com/migueldeicaza/SwiftTerm/pull/138#issuecomment-698693280

Sources/SwiftTerm/BufferSet.swift
@@ -67,14 +67,6 @@ class BufferSet {
         // Since the alt buffer is always cleared when the normal buffer is
         // activated, we want to fill it when switching to it.
         
-        // less switches to alt buffer without moving cursor and assumes
-        // switching to alt buffer resets buffer
-        alt.clear()
-        
-        // reset margins
-        alt.marginLeft = 0
-        alt.marginRight = alt.rows - 1
-        
         alt.fillViewportRows(attribute: fillAttr)
         active = alt
     }