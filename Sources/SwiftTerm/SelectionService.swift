migueldeicaza/SwiftTerm

commit 1cb0245a3a31291af09d2745d28572af28f9397b

Only raise the selection changed event if the active state did change

Sources/SwiftTerm/SelectionService.swift
@@ -36,8 +36,8 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+                terminal.tdel?.selectionChanged (source: terminal)
             }
-            terminal.tdel?.selectionChanged (source: terminal)
             if active == false {
                 pivot = nil
             }

commit 9f56c4d6ccd6a42ae9bf8e5d26ded4be1ce3c23f

Fix the regression where the selection was no longer being drawn, introduced this regression in 1cb0245a3a31291af09d2745d28572af28f9397b, fixes bug #246

Sources/SwiftTerm/SelectionService.swift
@@ -36,6 +36,10 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+            }
+            // When we set active to true from inside the temrinal, we rely on selectionChanged being raised
+            // because the boundaries changed.
+            if active {
                 terminal.tdel?.selectionChanged (source: terminal)
             }
             if active == false {