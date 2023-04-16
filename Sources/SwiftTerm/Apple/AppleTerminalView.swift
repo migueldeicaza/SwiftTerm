10c196213dc0d82601bb16442e1c7a426eba09c6
Miguel de Icaza <miguel@gnome.org>
2023-04-16 15:47:27 -0400

    This different approach at dealing with block cursors covering their content, this is achieved by replicating some of the code for drawing the terminal contents into the cursor code itself.
     While this works, I need to wire up both colors for a cursor (the foreground and background) so that this works correctly.

--- Sources/SwiftTerm/Apple/AppleTerminalView.swift
+++ Sources/SwiftTerm/Apple/AppleTerminalView.swift
@@ -92,7 +92,7 @@ extension TerminalView {
         
         // Install carret view
         if caretView == nil {
-            caretView = CaretView(frame: CGRect(origin: .zero, size: CGSize(width: cellDimension.width, height: cellDimension.height)), cursorStyle: terminal.options.cursorStyle)
+            caretView = CaretView(frame: CGRect(origin: .zero, size: CGSize(width: cellDimension.width, height: cellDimension.height)), cursorStyle: terminal.options.cursorStyle, terminal: self)
             addSubview(caretView)
         } else {
             updateCaretView ()
@@ -488,7 +488,8 @@ extension TerminalView {
     {
         let lineDescent = CTFontGetDescent(fontSet.normal)
         let lineLeading = CTFontGetLeading(fontSet.normal)
-
+        let yOffset = ceil(lineDescent+lineLeading)
+        
         func calcLineOffset (forRow: Int) -> CGFloat {
             cellDimension.height * CGFloat (forRow-bufferOffset+1) + offset
         }
@@ -582,7 +583,7 @@ extension TerminalView {
                 }
 
                 var positions = runGlyphs.enumerated().map { (i: Int, glyph: CGGlyph) -> CGPoint in
-                    CGPoint(x: lineOrigin.x + (cellDimension.width * CGFloat(col + i)), y: lineOrigin.y + ceil(lineLeading + lineDescent))
+                    CGPoint(x: lineOrigin.x + (cellDimension.width * CGFloat(col + i)), y: lineOrigin.y + yOffset)
                 }
 
                 var backgroundColor: TTColor?
@@ -803,6 +804,7 @@ extension TerminalView {
         let lineOrigin = CGPoint(x: 0, y: frame.height - offset)
         #endif
         caretView.frame.origin = CGPoint(x: lineOrigin.x + (cellDimension.width * doublePosition * CGFloat(buffer.x)), y: lineOrigin.y)
+        caretView.setText (ch: buffer.lines [vy][buffer.x])
     }
     
     // Does not use a default argument and merge, because it is called back
--- Sources/SwiftTerm/Mac/MacCaretView.swift
+++ Sources/SwiftTerm/Mac/MacCaretView.swift
@@ -11,16 +11,84 @@ import Foundation
 import AppKit
 import CoreText
 import CoreGraphics
+import CoreText
 
 // The CaretView is used to show the cursor
-class CaretView: NSView {
+class CaretView: NSView, CALayerDelegate {
+    weak var terminal: TerminalView?
     var sub: CALayer
+    var ctline: CTLine?
+    var backgroundColor: CGColor?
     
-    public init (frame: CGRect, cursorStyle: CursorStyle)
+    func getAttributes (_ attribute: Attribute) -> [NSAttributedString.Key:Any]?
     {
+        guard let terminal else {
+            return nil
+        }
+        let flags = attribute.style
+        var bg = attribute.bg
+        var fg = attribute.fg
+        
+        if flags.contains (.inverse) {
+            swap (&bg, &fg)
+            
+            if fg == .defaultColor {
+                fg = .defaultInvertedColor
+            }
+            if bg == .defaultColor {
+                bg = .defaultInvertedColor
+            }
+        }
+        
+        var tf: TTFont
+        let isBold = flags.contains(.bold)
+        if isBold {
+            if flags.contains (.italic) {
+                tf = terminal.fontSet.boldItalic
+            } else {
+                tf = terminal.fontSet.bold
+            }
+        } else if flags.contains (.italic) {
+            tf = terminal.fontSet.italic
+        } else {
+            tf = terminal.fontSet.normal
+        }
+        
+        // TODO: let fgColor = mapColor (color: fg, isFg: true, isBold: isBold)
+        let nsattr: [NSAttributedString.Key:Any] = [
+            .font: tf,
+            .foregroundColor: NSColor.black,
+            .backgroundColor: TTColor.make(color: terminal.terminal.cursorColor ?? Color (red: 0xffff, green: 0xffff, blue: 0xffff))
+        ]
+//        if flags.contains (.underline) {
+//            nsattr [.underlineColor] = fgColor
+//            nsattr [.underlineStyle] = NSUnderlineStyle.single.rawValue
+//        }
+//        if flags.contains (.crossedOut) {
+//            nsattr [.strikethroughColor] = fgColor
+//            nsattr [.strikethroughStyle] = NSUnderlineStyle.single.rawValue
+//        }
+//        if withUrl {
+//            nsattr [.underlineStyle] = NSUnderlineStyle.single.rawValue | NSUnderlineStyle.patternDash.rawValue
+//            nsattr [.underlineColor] = fgColor
+//
+//            // Add to cache
+//            urlAttributes [attribute] = nsattr
+//        } else {
+//            // Just add to cache
+//            attributes [attribute] = nsattr
+//        }
+        return nsattr
+    }
+    
+    public init (frame: CGRect, cursorStyle: CursorStyle, terminal: TerminalView)
+    {
+        self.terminal = terminal
         style = cursorStyle
+        //style = .steadyBlock
         sub = CALayer ()
         super.init(frame: frame)
+        //sub.delegate = self
         wantsLayer = true
         layer?.addSublayer(sub)
         
@@ -29,6 +97,15 @@ class CaretView: NSView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setText (ch: CharData) {
+        var res = NSAttributedString (
+            string: String (ch.getCharacter()),
+            attributes: getAttributes(ch.attribute))
+        ctline = CTLineCreateWithAttributedString(res)
+
+        setNeedsDisplay(bounds)
     }
     
     var style: CursorStyle {
@@ -41,16 +118,16 @@ class CaretView: NSView {
         switch style {
         case .blinkUnderline, .blinkBlock, .blinkBar:
             let anim = CABasicAnimation.init(keyPath: #keyPath (CALayer.opacity))
-            anim.duration = 0.7
+            anim.duration = 1
             anim.autoreverses = true
             anim.repeatCount = Float.infinity
             anim.fromValue = NSNumber (floatLiteral: 1)
-            anim.toValue = NSNumber (floatLiteral: 0.3)
+            anim.toValue = NSNumber (floatLiteral: 0)
             anim.timingFunction = CAMediaTimingFunction (name: .easeInEaseOut)
-            sub.add(anim, forKey: #keyPath (CALayer.opacity))
+            layer?.add(anim, forKey: #keyPath (CALayer.opacity))
         case .steadyBar, .steadyBlock, .steadyUnderline:
-            sub.removeAllAnimations()
-            sub.opacity = 1
+            layer?.removeAllAnimations()
+            layer?.opacity = 1
         }
         
         guard let layer = self.layer else {
@@ -91,7 +168,56 @@ class CaretView: NSView {
         sub.frame = CGRect (origin: CGPoint.zero, size: layer.frame.size)
         sub.borderWidth = isFirst ? 0 : 1
         sub.borderColor = caretColor.cgColor
-        sub.backgroundColor = isFirst ? caretColor.cgColor : NSColor.clear.cgColor
+        setNeedsDisplay(bounds)
+        //sub.backgroundColor = isFirst ? caretColor.cgColor : NSColor.clear.cgColor
+        //sub.backgroundColor = NSColor.red.cgColor
+        backgroundColor = isFirst ? caretColor.cgColor : NSColor.clear.cgColor
+    }
+    
+    func draw(_ layer: CALayer, in context: CGContext) {
+        drawCursor (in: context)
+    }
+    
+    func drawCursor (in context: CGContext) {
+        guard let ctline else {
+            return
+        }
+        guard let terminal else {
+            return
+        }
+        let lineDescent = CTFontGetDescent(terminal.fontSet.normal)
+        let lineLeading = CTFontGetLeading(terminal.fontSet.normal)
+        let yOffset = ceil(lineDescent+lineLeading)
+
+        if let backgroundColor {
+            context.setFillColor(backgroundColor)
+            context.fill([bounds])
+        }
+        context.setFillColor(NSColor.black.cgColor)
+        for run in CTLineGetGlyphRuns(ctline) as? [CTRun] ?? [] {
+            let runGlyphsCount = CTRunGetGlyphCount(run)
+            let runAttributes = CTRunGetAttributes(run) as? [NSAttributedString.Key: Any] ?? [:]
+            let runFont = runAttributes[.font] as! TTFont
+
+            let runGlyphs = [CGGlyph](unsafeUninitializedCapacity: runGlyphsCount) { (bufferPointer, count) in
+                CTRunGetGlyphs(run, CFRange(), bufferPointer.baseAddress!)
+                count = runGlyphsCount
+            }
+
+            var positions = runGlyphs.enumerated().map { (i: Int, glyph: CGGlyph) -> CGPoint in
+                CGPoint(x: 0, y: yOffset)
+            }
+            CTFontDrawGlyphs(runFont, runGlyphs, &positions, positions.count, context)
+
+        }
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+        guard let context = NSGraphicsContext.current?.cgContext else {
+            return
+        }
+        //drawCursor(in: dirtyRect)
+        
     }
     
     override func hitTest(_ point: NSPoint) -> NSView? {
