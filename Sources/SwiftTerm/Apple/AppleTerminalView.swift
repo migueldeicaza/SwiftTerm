4df9211e2f039e426ba02859a7d668e40212d90f
Miguel de Icaza <miguel@gnome.org>
2023-07-20 06:48:17 -0400

    The height can be an infinite number or a NaN, not sure why this could ever happen after setting and limiting a font size between 5 and 72, but this prevents a crash

--- Sources/SwiftTerm/Apple/AppleTerminalView.swift
+++ Sources/SwiftTerm/Apple/AppleTerminalView.swift
@@ -147,7 +147,7 @@ extension TerminalView {
         let fontAttributes = [NSAttributedString.Key.font: fontSet.normal]
         let cellWidth = "W".size(withAttributes: fontAttributes).width
         #endif
-        return CellDimension(width: max (1, cellWidth), height: max (cellHeight, 1))
+        return CellDimension(width: max (1, cellWidth), height: max (min (cellHeight, 8192), 1))
     }
     
     func mapColor (color: Attribute.Color, isFg: Bool, isBold: Bool) -> TTColor
