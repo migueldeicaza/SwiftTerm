6543e6f88053719b48b299f15a1118c2afa88e9a
Miguel de Icaza <miguel@gnome.org>
2025-06-13 08:27:41 +0900

    Performance baseline

---
+++ Tests/SwiftTermTests/PerformanceTest.swift
@@ -0,0 +1,32 @@
+//
+//  PerformanceTest.swift
+//
+//
+//  Created by Miguel de Icaza on 4/17/20.
+//
+#if os(macOS)
+import Foundation
+import XCTest
+
+@testable import SwiftTerm
+
+final class PerformaceTests: XCTestCase {
+
+    func testPerformance() {
+        let h = HeadlessTerminal (queue: SwiftTermTests.queue) { exitCode in }
+        let t = h.terminal!
+        
+        
+        measure {
+            t.feed (text: "\u{1b}[38;2;19;49;174;48;2;23;56;179mStringThis is a very long line\n\r")
+            for x in 0..<10000 {
+                t.feed(text: "pointless repetition\n")
+            }
+        }
+    }
+    
+    static var allTests = [
+        ("testPerformance", testPerformance)
+    ]
+}
+#endif
