7899365c8f8c3223c28454a804a1677c85de8ba6
Miguel de Icaza <miguel@gnome.org>
2025-06-14 08:22:38 +0900

    Add another way of testing the speed, based on the Apple talk 'Optimize CPU performance with Instruments' from WWDC 2-25

--- Tests/SwiftTermTests/PerformanceTest.swift
+++ Tests/SwiftTermTests/PerformanceTest.swift
@@ -7,10 +7,11 @@
 #if os(macOS)
 import Foundation
 import XCTest
-
+import os
 @testable import SwiftTerm
 
 final class PerformaceTests: XCTestCase {
+    let signposter = OSSignposter(subsystem: "SwiftTerm", category: .pointsOfInterest)
 
     func testPerformance() {
         let h = HeadlessTerminal (queue: SwiftTermTests.queue) { exitCode in }
@@ -25,8 +26,36 @@ final class PerformaceTests: XCTestCase {
         }
     }
     
+    func testPerformance2() {
+        testFeed(duration: Duration(secondsComponent: 3, attosecondsComponent: 0))
+    }
+    
+    func testFeed(duration: Duration) {
+        let h = HeadlessTerminal (queue: SwiftTermTests.queue) { exitCode in }
+        let t = h.terminal!
+
+        var now = ContinuousClock.now
+        var outerIterations = 0
+        let internval = signposter.beginInterval("insertCharacter")
+        let start = ContinuousClock.now
+        
+        t.feed (text: "\u{1b}[38;2;19;49;174;48;2;23;56;179mStringThis is a very long line\n\r")
+
+        repeat {
+            t.feed(text: "pointless repetition\n")
+            outerIterations += 1
+            now = .now
+        } while (start.duration(to: now) < duration)
+        let elapsed = start.duration(to: now)
+        let attoseconds = Double(elapsed.components.attoseconds)
+        let seconds = Double(elapsed.components.seconds)
+        let throughput = Double(outerIterations) / (seconds + attoseconds / 1e18)
+        signposter.endInterval("insertCharacter", internval, "\(throughput) throughput calls/s")
+        print("insertCharacter: \(throughput) throughput calls/s")
+    }
     static var allTests = [
-        ("testPerformance", testPerformance)
+        ("testPerformance", testPerformance),
+        ("testPerformance2", testPerformance2)
     ]
 }
 #endif
