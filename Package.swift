ba959c2154c2b4028ec9993522d724cb3a0d1e49
Miguel de Icaza <miguel@gnome.org>
2026-01-12 18:23:53 -0500

    Without a fixed version, folks can not use it.   Fixes #438

--- Package.swift
+++ Package.swift
@@ -92,7 +92,7 @@ let package = Package(
     products: products,
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/swiftlang/swift-subprocess", branch: "main")
+        .package(url: "https://github.com/swiftlang/swift-subprocess", revision: "426790f3f24afa60b418450da0afaa20a8b3bdd4")
     ],
     targets: targets,
     swiftLanguageVersions: [.v5]
