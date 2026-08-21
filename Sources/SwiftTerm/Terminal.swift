d879d4d01dd23a6813fd7de81108874c54d3565a
Paul Furgale <pfurgale@nvidia.com>
2026-08-21 18:39:47 +0200

    Exclude terminal padding from implicit links

--- Sources/SwiftTerm/Terminal.swift
+++ Sources/SwiftTerm/Terminal.swift
@@ -7423,7 +7423,6 @@ open class Terminal {
         let pathChars = #"[\w\-.~:\/?#@!$&*+;=%]"#
         let noTrailingPunctuation = #"(?<![,.])"#
         let noTrailingColon = #"(?<!:)"#
-        let trailingSpacesAtEOL = #"(?: +(?= *$))?"#
         let dottedPathLookahead = #"(?=[\w\-.~:\/?#@!$&*+;=%]*\.)"#
         let nonDottedPathLookahead = #"(?![\w\-.~:\/?#@!$&*+;=%]*\.)"#
         let dottedPathSpaceSegments = #"(?:(?<!:) (?!\w+:\/\/)[\w\-.~:\/?#@!$&*+;=%]*[\/.])*"#
@@ -7459,13 +7458,11 @@ open class Terminal {
             pathChars + "+" +
             dottedPathSpaceSegments +
             noTrailingColon +
-            trailingSpacesAtEOL +
             "|" +
             nonDottedPathLookahead +
             pathChars + "+" +
             anyPathSpaceSegments +
             noTrailingColon +
-            trailingSpacesAtEOL +
             ")"
 
         // Ghostty uses (?<!\$\d*) here, which is unsupported by ICU.
@@ -7476,8 +7473,7 @@ open class Terminal {
             bareRelativePathPrefix +
             pathChars + "+" +
             dottedPathSpaceSegments +
-            noTrailingColon +
-            trailingSpacesAtEOL
+            noTrailingColon
 
         let regex = schemeURLBranch + "|" + rootedOrRelativePathBranch + "|" + bareRelativePathBranch
         return try? NSRegularExpression(pattern: regex, options: [])
